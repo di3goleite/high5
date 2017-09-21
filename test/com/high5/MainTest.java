46ec646bb0754612ec4c1caba49e711db17784ae
Diego Leite <diegojleite@gmail.com>
2017-09-21 06:42:55 -0300

    Tests working

---
+++ test/com/high5/MainTest.java
@@ -0,0 +1,13 @@
+package com.high5;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void helloWorld() {
+        Assert.assertTrue(true);
+    }
+}
